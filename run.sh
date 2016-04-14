2115626f92d7bc2912b92e7fae69c439f1c6b223
hexbot <cachalot1984@163.com>
2016-04-14 09:18:37 +0800

    add start script

---
+++ run.sh
@@ -0,0 +1,18 @@
+#
+# Start hexblog if not started already
+# On Netease cloud container, add crontab task to run it periodically:
+# 	# crontab -e
+#	add line '* *    *   *   *   /work/hexblog/run.sh'
+#
+
+
+HEXBLOG_ROOT='/work/hexblog'
+HEXBLOG_LOG_FILE="${HEXBLOG_ROOT}/log"
+
+
+if [ `ps -ef | grep python | grep manage.py | wc -l` -lt 1 ]; then
+	cd ${HEXBLOG_ROOT}
+	./manage.py runserver --host 0.0.0.0 --port 80 > ${HEXBLOG_LOG_FILE} 2>&1 &
+fi
+
+
